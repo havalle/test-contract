479f83de2cdc195e16adf579ef31663528cc0cef
Hernan Avalle <havalle@gmail.com>
2022-02-02 21:21:31 +0100

    Test deployment for Gteetings sample contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import { ethers } from "ethers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        alert('Please install MetaMask!');
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+
+      console.log('Connected', accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchGreetings = async () => {
+    let contractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+    const {ethereum} = window;
+
+    if (!ethereum) {
+      alert('Please install MetaMask!');
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const contract = new ethers.Contract(
+      contractAddress,
+      Greeter.abi,
+      provider
+    );
+
+    const greeting = await contract.greet();
+    console.log(greeting);
+  }
+
+  const sendGreetings = async () => {
+    let contractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+    const {ethereum} = window;
+
+    if (!ethereum) {
+      alert('Please install MetaMask!');
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress,
+      Greeter.abi,
+      signer
+    );
+
+    await contract.setGreeting('Hello Smart Contracts NEW!!')
+  }
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
+  return <div>
+    <button onClick={() => fetchGreetings()}>READ</button>
+    <button onClick={() => sendGreetings()}>SAVE</button>
+  </div>;
 }
-
-export default App;
